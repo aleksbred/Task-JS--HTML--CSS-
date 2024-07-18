989bfd62d51db506ddb83e7beb096578d2c13a6e
aleksbred <aleksbred2024@gmail.com>
2024-07-18 04:31:15 +0300

    switcher-themes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,31 @@ import { headerContainer, listContainer, submitBtn, numberQuestion, numberQuesti
 import { singleQuestionTemplate, multipleQuestionTemplate } from './template.js';
 import { questions } from './questions.js';
 
+document.addEventListener("DOMContentLoaded", ()=>{
+    init()
+  })
+  function init() {
+    if(localStorage.getItem('theme')) {
+      document.documentElement.setAttribute("theme", "dark");
+    }
+    else {
+      document.documentElement.removeAttribute("theme");
+    }
+    
+  }
+  const toggleBtn = document.querySelector("#toggle-theme");
+  toggleBtn.addEventListener("click", function() {
+    if(document.documentElement.hasAttribute("theme")){
+      document.documentElement.removeAttribute("theme");
+      localStorage.removeItem('theme');
+    }
+    else{
+      document.documentElement.setAttribute("theme", "dark");
+      localStorage.setItem('theme', 1);
+    }
+  });
+
+
 let score = 0;
 let questionIndex = 0;
 
